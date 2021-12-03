1044df33f368a49a966d03b457c865ccd0a6a88a
Crunk <jb10051075@gmail.com>
2021-12-03 11:06:03 +0100

    Usestate
    - Filter over item with key
    - Delete item from state with click event

--- src/App.js
+++ src/App.js
@@ -2,30 +2,50 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-	const [name, setName] = useState('mario');
+	const [showEvents, setShowEvents] = useState(true);
+
 	const [events, setEvents] = useState([
 		{ title: 'Mario bash boy', id: 1 },
 		{ title: 'Luigi Will be your best friend', id: 2 },
 		{ title: 'Peach is the princes of the game', id: 3 },
 	]);
 
-	const handleClick = () => {
-		setName('Luigi');
-		console.log(name);
+	console.log(showEvents);
+
+	const handleClick = (id) => {
+		// * Delete previous value of the updates state event (which will change in the future) from the State
+		// ! Access the previous state first (Best practice)
+		setEvents((prevEvents) => {
+			return prevEvents.filter((event) => {
+				return id !== event.id;
+			});
+		});
+		console.log(id);
 	};
 
 	return (
 		<div className='App'>
-			<h1>My name is {name}</h1>
-			<button onClick={handleClick}> Change name</button>
-
-			{events.map((event, i) => (
-				<div key={event.id} class>
-					<h2>
-						{i} - {event.title}
-					</h2>
+			{showEvents && ( // * Show event if true
+				<div>
+					<button onClick={() => setShowEvents(false)}> Hide Event</button>
 				</div>
-			))}
+			)}
+
+			{!showEvents && ( // * Hide event if false
+				<div>
+					<button onClick={() => setShowEvents(true)}>Show Events</button>
+				</div>
+			)}
+
+			{showEvents && // ! Hide events if state condition is false
+				events.map((event, i) => (
+					<div key={event.id}>
+						<h2>
+							{i} - {event.title}
+						</h2>
+						<button onClick={() => handleClick(event.id)}>Delete Event</button>
+					</div>
+				))}
 		</div>
 	);
 }
