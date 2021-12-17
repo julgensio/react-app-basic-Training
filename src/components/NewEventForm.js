9bb8a9ec5792aeb267eaa7b7998fc937f4057104
Crunk <jb10051075@gmail.com>
2021-12-17 18:17:06 +0100

    Controlled input
    -Added reset input form

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -5,20 +5,34 @@ export default function NewEventForm() {
 	const [title, setTitle] = useState('');
 	const [date, setDate] = useState('');
 
+	const resetForm = () => {
+		setTitle('');
+		setDate('');
+	};
+
 	return (
 		<form className='new-event-form'>
 			<label>
 				<span>Event Title:</span>
-				<input type='text' onChange={(e) => setTitle(e.target.value)} />
+				<input
+					type='text'
+					onChange={(e) => setTitle(e.target.value)} // * Update state with the given title
+					value={title} // * Reset input
+				/>
 			</label>
 			<label>
 				<span>Event Date:</span>
-				<input type='date' onChange={(e) => setDate(e.target.value)} />
-			</label> 
+				<input
+					type='date'
+					onChange={(e) => setDate(e.target.value)} // * Update state with the given date
+					value={date} // * Reset input
+				/>
+			</label>
 			<button>Submit</button>
 			<p>
 				Title - {title}, Date - {date}
 			</p>
+			<p onClick={resetForm}> reset form </p>
 		</form>
 	);
 }
